1e03f06bf74f39db535fc367a9d314ff592bf5c3
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-05-04 13:25:25 +0200

    fix

--- 5 - Replacing, Transforming and Cleaning Data/example_masking_sensitive_data.py
+++ 5 - Replacing, Transforming and Cleaning Data/example_masking_sensitive_data.py
@@ -8,12 +8,13 @@ Credit Card:t 1234 5678 9012 3456
 """
 
 email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
-phone_pattern = r"\+?\d{1,4}[-.() \d]*\d"
 credit_card_pattern = r"\b(?:\d[ -]*?){13,16}\b"
+phone_pattern = r"\+?\d{1,4}[-.() \d]*\d"
 
 redacted_text = re.sub(email_pattern, "[REDACTED_EMAIL]", text)
-redacted_text = re.sub(phone_pattern, "[REDACTED_PHONE]", redacted_text)
 redacted_text = re.sub(credit_card_pattern, "[REDACTED_CC]", redacted_text)
+redacted_text = re.sub(phone_pattern, "[REDACTED_PHONE]", redacted_text)
+
 
 print("Redacted text:")
 print(redacted_text)
